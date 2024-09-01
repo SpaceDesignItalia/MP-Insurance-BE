e8524d65676b2ab03176fd0dc48ab6ac272cfeb3
Marco Macherelli <elmache05@gmail.com>
2024-09-01 13:32:32 +0200

    Delete Customer and minor fixes

--- Routes/Customer/Customer.js
+++ Routes/Customer/Customer.js
@@ -4,11 +4,13 @@ const router = express.Router();
 const customerPOST = require("./customerPOST");
 const customerGET = require("./customerGET");
 const customerUPDATE = require("./customerUPDATE");
+const customerDELETE = require("./customerDELETE");
 
 const Customer = (db) => {
   router.use("/POST", customerPOST(db));
   router.use("/GET", customerGET(db));
   router.use("/UPDATE", customerUPDATE(db));
+  router.use("/DELETE", customerDELETE(db));
 
   return router;
 };
